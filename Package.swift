79570ce3dd2d8f28546a69c09feba91c51a85e83
timominous <timominous@gmail.com>
2016-02-16 16:22:35 +0800

    Added Fluent dependency

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 let package = Package(
   name: "SwiftServer",
   dependencies: [
-    .Package(url: "https://github.com/loganwright/vapor.git", majorVersion: 0)
+    .Package(url: "https://github.com/loganwright/vapor.git", majorVersion: 0),
+    .Package(url: "https://github.com/tannernelson/fluent.git", majorVersion: 0)
   ]
 )

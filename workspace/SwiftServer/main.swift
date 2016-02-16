c6be16ad68f1abb15a3d72f5b9660a118710138b
timominous <timominous@gmail.com>
2016-02-16 15:24:12 +0800

    Initial commit

---
+++ workspace/SwiftServer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftServer
+//
+//  Created by Tim on 16/02/2016.
+//  Copyright © 2016 timominous. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
